1dfd0dba2e03eb4dc413c20fc0a9821e7712c897
Eduardo Corona <eduardo.corona040@gmail.com>
2024-06-25 01:02:33 +0100

    Update ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -5,9 +5,30 @@ int	main(int argc, char **argv) {
 	std::ofstream	outfile;
 	std::ifstream	infile;
 	std::string		input;
+	std::string		file;
+	std::string		s1;
+	std::string		s2;
+	size_t			cpos;
 
 	if (argc != 4)
 		return (0);
+	file = argv[1];
 	infile.open(argv[1]);
-	infile >> input;
+	outfile.open((file + ".replace").c_str());
+	s1 = argv[2];
+	s2 = argv[3];
+	if (infile.is_open()) {
+		getline(infile, input);
+		while (infile) {
+			while ((cpos = input.find(s1)) != std::string::npos) {
+				outfile << input.substr(0, cpos);
+				outfile << s2;
+				input = input.substr(cpos + s1.length(), std::string::npos);
+			}
+			outfile << input << std::endl;
+			getline(infile, input);
+		}
+	}
+	infile.close();
+	outfile.close();
 }
