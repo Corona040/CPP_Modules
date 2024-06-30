da9b6142c312af54485ee409f2beccb2547a5320
Eduardo Corona Oliveira Costa <ecorona-@c1r6s6.42porto.com>
2024-06-30 15:07:56 +0100

    Done CPP02

--- CPP02/ex03/Point.cpp
+++ CPP02/ex03/Point.cpp
@@ -4,16 +4,16 @@ Point::Point(void): x(0), y(0) {}
 
 Point::Point(const float x, const float y): x(x), y(y) {}
 
-Point::Point(const Point &p) {}
+Point::Point(const Point &p): x(p.x), y(p.y) {}
 
-Point &(Point::operator=)(const Point &p) {return (*this);}
+Point &(Point::operator=)(const Point &p) {(void)p;return (*this);}
 
 Point::~Point(void) {}
 
-const Fixed	&Point::getx(void) {
+const Fixed	&Point::getx(void) const {
 	return (this->x);
 }
 
-const Fixed	&Point::gety(void) {
+const Fixed	&Point::gety(void) const {
 	return (this->y);
 }
--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -1,5 +1,21 @@
 #include "Point.hpp"
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point) {
-	return (false);
+	bool		is_inside = true;
+	const int	n_vertices = 3;
+	const Point	vs[] = {a, b, c};
+	Fixed		m;
+	Fixed		yk;
+	Fixed		yp;
+
+	for (int i = 0; i < n_vertices - 1; i++) {
+		for (int j = i + 1; j < n_vertices; j++) {
+			int k = n_vertices - i - j;
+			m = (vs[j].gety() - vs[i].gety()) / (vs[j].getx() - vs[i].getx());
+			yk = (vs[k].getx() - vs[i].getx()) * m + vs[i].gety();
+			yp = (point.getx() - vs[i].getx()) * m + vs[i].gety();
+			is_inside = is_inside && (vs[k].gety() > yk == point.gety() > yp);
+		}
+	}
+	return (is_inside);
 }
